5a94e210232bf558e36c6f7a5cc056ffcdf95541
LiuHuiGitHub <457344370@qq.com>
2016-04-23 14:06:53 +0800

    整理文件

---
+++ trunk/App/app_test.h
@@ -0,0 +1,16 @@
+#ifndef __APP_TEST_H__
+#define __APP_TEST_H__
+
+#include "typedef.h"
+#include "app_adc.h"
+
+extern UINT8 OverCurrentChannelFlag[AD_CHANNEL_NUM];
+extern BOOL b_TestMode;
+
+void app_testInit(void);
+void app_testOverCurrentInit(void);
+void app_testOverCurrentProtection(void);
+void app_testOverCurrentRecoveryHandler10s(void);
+void app_testCurrentTest1s(void);
+
+#endif
